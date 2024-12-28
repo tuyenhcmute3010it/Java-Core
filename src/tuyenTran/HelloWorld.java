622bf233613d073e3cb1a79e8d4fbd83ce0d1e81
tuyen <tuyenbest1234@gmail.com>
2024-12-28 22:53:08 +0700

    Init Project

---
+++ src/tuyenTran/HelloWorld.java
@@ -0,0 +1,10 @@
+package tuyenTran;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World With Java");
+		System.out.println("Hello World With Java");
+		System.out.println("Hello World With Java");
+		System.out.println("Hello World With Java");
+	}
+}
